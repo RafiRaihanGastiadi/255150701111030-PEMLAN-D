e27954b528b0d7c72bbf5573201f3361fb57df52
RafiRaihanGastiadi <rafiraihan0507@gmail.com>
2026-04-21 20:04:44 +0700

    Add files via upload

---
+++ Modul 8/MainRectangle.java
@@ -0,0 +1,52 @@
+interface Colorable {
+    void howToColor();
+}
+
+interface Comparable { 
+    void compareTo(Object obj);
+}
+
+// Class Rectangle mengimplementasikan dua interface
+class Rectangle implements Colorable, Comparable {
+    private String warna;
+    private int kategori;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(String warna) {
+        this.warna = warna;
+    }
+
+    @Override
+    public void howToColor() {
+        if (this.warna == null) {
+            System.out.println("Tidak ada warna, warna bangun kotak masih polos");
+        } else {
+            System.out.println("Bangun kotak sudah diwarnai dengan warna " + this.warna);
+        }
+    }
+
+    @Override
+    public void compareTo(Object obj) {
+        // Casting dari Object ke int
+        this.kategori = (int) obj;
+        if (this.kategori == 0) {
+            System.out.println("Ukuran cat yang cocok untuk bangun kotak dengan ukuran kategori " + this.kategori + " yaitu 2.5L");
+        } else {
+            System.out.println("Ukuran cat yang cocok untuk bangun kotak dengan ukuran kategori " + this.kategori + " yaitu 6.5L");
+        }
+    }
+}
+
+public class MainRectangle {
+    public static void main(String[] args) {
+        Rectangle kotak1 = new Rectangle("merah");
+        Rectangle kotak2 = new Rectangle();
+        Rectangle kotak3 = new Rectangle();
+
+        kotak1.howToColor();
+        kotak2.howToColor();
+        kotak3.compareTo(4);
+    }
+}
